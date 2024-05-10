4b756c9deeed904522414c35f6cadb96fa64756b
Carlos Dueñas <duenascarlos9@gmail.com>
2024-05-09 23:28:55 -0500

    Arreglos de informacion de apartados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { AuthServiceService } from './core/services/autentificacion/auth-service.service';
@@ -13,7 +13,7 @@ import { AuthServiceService } from './core/services/autentificacion/auth-service
 })
 export class AppComponent {
   title = 'frontend';
-  constructor(private authService: AuthServiceService){}
+  constructor(private authService: AuthServiceService, private router: Router){}
   isLoggedIn(): boolean {
     // Fetch the login status from AuthServiceService
     return this.authService.isLoggedIn();
@@ -26,5 +26,6 @@ export class AppComponent {
   logout(){
     this.authService.deleteToken()
     console.log(this.authService.getToken)
+    this.router.navigate(['']);
   }
 }
